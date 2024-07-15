31397e4e1c074c46a787b8866b0df3ed702464a9
KgothatsoTheko <kgothatsotheko7@gmail.com>
2024-07-15 11:11:16 +0200

    IDS 3

--- server/src/sd-services/receipt_Data.ts
+++ server/src/sd-services/receipt_Data.ts
@@ -132,38 +132,6 @@ export class receipt_Data {
             next
           );
           let parentSpanInst = null;
-          bh = await this.sd_oAIXs1g6MtIZlgDJ(bh, parentSpanInst);
-          //appendnew_next_sd_32rFD19v7gcBKDMU
-        } catch (e) {
-          return await this.errorHandler(bh, e, 'sd_32rFD19v7gcBKDMU');
-        }
-      },
-      this.sdService.getMiddlesWaresBySequenceId(
-        null,
-        'post',
-        this.generatedMiddlewares
-      )
-    );
-
-    this.app['get'](
-      `${this.serviceBasePath}/get-receipt-data`,
-      cookieParser(),
-      this.sdService.getMiddlesWaresBySequenceId(
-        null,
-        'pre',
-        this.generatedMiddlewares
-      ),
-
-      async (req, res, next) => {
-        let bh: any = {};
-        try {
-          bh = this.sdService.__constructDefault(
-            { local: {}, input: {} },
-            req,
-            res,
-            next
-          );
-          let parentSpanInst = null;
           bh = await this.sd_fyuH8bSVxuO0uo7j(bh, parentSpanInst);
           //appendnew_next_sd_LITkUuuJlYXmnDvY
         } catch (e) {
@@ -246,70 +214,6 @@ export class receipt_Data {
       return bh;
     } catch (e) {
       return await this.errorHandler(bh, e, 'sd_dYMiPVLrPaRRhOnO');
-    }
-  }
-
-  async sd_oAIXs1g6MtIZlgDJ(bh, parentSpanInst) {
-    const spanInst = this.tracerService.createSpan(
-      'sd_oAIXs1g6MtIZlgDJ',
-      parentSpanInst
-    );
-    try {
-      bh.receiptData = {
-        collection: 'ReceiptData',
-      };
-
-      bh.input.body['Date-Uploaded'] = new Date().toLocaleDateString();
-      this.tracerService.sendData(spanInst, bh);
-      bh = await this.sd_iNNNA6psRGdvVtM5(bh, parentSpanInst);
-      //appendnew_next_sd_oAIXs1g6MtIZlgDJ
-      return bh;
-    } catch (e) {
-      return await this.errorHandler(
-        bh,
-        e,
-        'sd_oAIXs1g6MtIZlgDJ',
-        spanInst,
-        'sd_oAIXs1g6MtIZlgDJ'
-      );
-    }
-  }
-
-  async sd_iNNNA6psRGdvVtM5(bh, parentSpanInst) {
-    const spanInst = this.tracerService.createSpan(
-      'sd_iNNNA6psRGdvVtM5',
-      parentSpanInst
-    );
-    try {
-      bh.result = await MongoPersistance.getInstance().find(
-        'sd_qC5Zfy7LJYHmxiFv',
-        bh.receiptData.collection,
-        bh.opt,
-        bh.option,
-        bh.option
-      );
-      this.tracerService.sendData(spanInst, bh);
-      await this.sd_P1AwM7n0uPqyUNwU(bh, parentSpanInst);
-      //appendnew_next_sd_iNNNA6psRGdvVtM5
-      return bh;
-    } catch (e) {
-      return await this.errorHandler(
-        bh,
-        e,
-        'sd_iNNNA6psRGdvVtM5',
-        spanInst,
-        'sd_iNNNA6psRGdvVtM5'
-      );
-    }
-  }
-
-  async sd_P1AwM7n0uPqyUNwU(bh, parentSpanInst) {
-    try {
-      bh.web.res.status(200).send(bh.result);
-
-      return bh;
-    } catch (e) {
-      return await this.errorHandler(bh, e, 'sd_P1AwM7n0uPqyUNwU');
     }
   }
 
